cf54a655b1d831915476d11932eb80a892c28946
Ron Waldon <jokeyrhyme@gmail.com>
2015-06-09 16:13:05 +1000

    confirm to JavaScript Semi-Standard

--- index.js
+++ index.js
@@ -1,23 +1,23 @@
 /*eslint-disable no-process-exit, no-sync*/ // this is a CLI tool
 
-"use strict";
+'use strict';
 
 // Node.js built-ins
 
-var fs = require("fs");
-var path = require("path");
-var url = require("url");
+var fs = require('fs');
+var path = require('path');
+var url = require('url');
 
 // 3rd-party modules
 
-var Fetcher = require("appcache-fetcher");
+var Fetcher = require('appcache-fetcher');
 
-var program = require("commander");
-var rimraf = require("rimraf");
+var program = require('commander');
+var rimraf = require('rimraf');
 
 // this module
 
-var pkg = require("./package.json");
+var pkg = require('./package.json');
 
 var fetcher;
 
@@ -26,12 +26,12 @@ var outputPath;
 
 program
 .version(pkg.version)
-.arguments("<url> <output>")
+.arguments('<url> <output>')
 .action(function (rUrl, output) {
   remoteUrl = rUrl;
   outputPath = output;
 })
-.option("-f, --force", "wipe `output' if it already exists, create if needed")
+.option('-f, --force', 'wipe `output\' if it already exists, create if needed')
 .parse(process.argv);
 
 (function () {
@@ -43,7 +43,7 @@ program
   }
 
   if (!parsed || !parsed.protocol || !parsed.host) {
-    console.error("error: unable to parse `url'");
+    console.error('error: unable to parse `url\'');
     program.help();
     process.exit(1);
   }
@@ -51,7 +51,7 @@ program
 
 if (fs.existsSync(outputPath)) {
   if (!program.force) {
-    console.error("error: `output' exists: " + outputPath);
+    console.error('error: `output\' exists: ' + outputPath);
     program.help();
     process.exit(1);
   }
@@ -61,18 +61,18 @@ if (fs.existsSync(outputPath)) {
 
 fetcher = new Fetcher({ remoteUrl: remoteUrl, localPath: outputPath });
 
-fetcher.addExtractor("manifestUrl", require("./lib/extractors/manifestUrl.dataAttribute"));
+fetcher.addExtractor('manifestUrl', require('./lib/extractors/manifestUrl.dataAttribute'));
 
 // these are in REVERSE order because they insert before the first SCRIPT
-fetcher.addTransform("html", require("./lib/transforms/html.injectBlinkGap"));
-fetcher.addTransform("html", require("./lib/transforms/html.injectCordova"));
-fetcher.addTransform("html", require("./lib/transforms/html.injectWinJsCompat"));
-fetcher.addTransform("html", require("./lib/transforms/html.injectWinJs"));
+fetcher.addTransform('html', require('./lib/transforms/html.injectBlinkGap'));
+fetcher.addTransform('html', require('./lib/transforms/html.injectCordova'));
+fetcher.addTransform('html', require('./lib/transforms/html.injectWinJsCompat'));
+fetcher.addTransform('html', require('./lib/transforms/html.injectWinJs'));
 
 fetcher.go()
 .then(function () {
-  var input = path.join(__dirname, "vendor", "winstore-jscompat.js");
-  var output = path.join(outputPath, "winstore-jscompat.js");
+  var input = path.join(__dirname, 'vendor', 'winstore-jscompat.js');
+  var output = path.join(outputPath, 'winstore-jscompat.js');
   fs.createReadStream(input)
   .pipe(fs.createWriteStream(output));
 });
